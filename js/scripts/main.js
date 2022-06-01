b75ddacc204e5cc36393f4142eeb277a1fbecf40
Wagner Paulo <wagstalos@hotmail.com>
2022-06-01 09:03:31 -0300

    list pokemons

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -24,14 +24,50 @@ btnDropdownSelect.addEventListener("click", () => {
   btnDropdownSelect.parentElement.classList.toggle("active");
 });
 
-const areaPokemons = document.getElementById("js-list-pokemons");
+const areaPokemons = document.getElementById('js-list-pokemons');
 
-function openDetailsPokemon() {
-  document.documentElement.classList.add("open-modal");
+function fisrtLetter(string){
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function closeDetailsPokemon() {
-  document.documentElement.classList.remove("open-modal");
+function createCardPokemon(code, type, nome, imagePok) {
+  let card = document.createElement("button");
+  card.classList = `card-pokemon js-open-details-pokemon ${type}`;
+  areaPokemons.appendChild(card);
+
+  let image = document.createElement("div");
+  image.classList = 'image';
+  card.appendChild(image);
+
+  let imageSrc = document.createElement('img');
+  imageSrc.className = 'thumb-img'
+  imageSrc.setAttribute('src', imagePok);
+  image.appendChild(imageSrc);
+
+  let infoCardPokemon = document.createElement('div');
+  infoCardPokemon.classList = 'info';
+  card.appendChild(infoCardPokemon);
+
+  let infoTextPokemon = document.createElement('div');
+  infoTextPokemon.classList = 'text';
+  infoCardPokemon.appendChild(infoTextPokemon);
+
+  let codePokemon = document.createElement('span')
+  codePokemon.textContent = (code < 10 ) ? `#00${code}` : (code < 100) ? `#0${code}` :  `#${code}`; 
+  infoTextPokemon.appendChild(codePokemon);
+
+  let namePokemon = document.createElement('h3');
+  namePokemon.textContent = fisrtLetter(nome);
+  infoTextPokemon.appendChild(namePokemon);
+
+  let areaIcon = document.createElement('div');
+  areaIcon.classList = 'icon';
+  infoCardPokemon.appendChild(areaIcon);
+
+  let imgType = document.createElement('img');
+  imgType.setAttribute('src', `img/icon-types/${type}.svg`)
+  areaIcon.appendChild(imgType);
+
 }
 
 function listinungPokemons(urlApi) {
@@ -44,12 +80,56 @@ function listinungPokemons(urlApi) {
 
     countPokemons.innerText = count;
 
-    results.forEach(pokemon =>{
+    results.forEach((pokemon) => {
       let urlApiDetails = pokemon.url;
 
-      console.log(urlApiDetails);
-    })
+      axios({
+        method: "GET",
+        url: `${urlApiDetails}`,
+      }).then((response) => {
+        const { name, id, sprites, types } = response.data;
+
+        const infoCard = {
+          nome: name,
+          code: id,
+          image: sprites.other.dream_world.front_default,
+          type: types[0].type.name,
+        };
+
+        createCardPokemon(
+          infoCard.code,
+          infoCard.type,
+          infoCard.nome,
+          infoCard.image
+        );
+
+        const cardPokemon = document.querySelectorAll('.js-open-details-pokemon');
+
+        cardPokemon.forEach(card =>{
+          card.addEventListener('click', openDetailsPokemon )
+        })
+      });
+    });
   });
 }
 
 listinungPokemons("https://pokeapi.co/api/v2/pokemon?limit=9&offset=0");
+
+
+function openDetailsPokemon() {
+  document.documentElement.classList.add("open-modal");
+}
+
+function closeDetailsPokemon() {
+  document.documentElement.classList.remove("open-modal");
+}
+
+
+//script para listar todos os tipos de pokemon
+
+const areaTypes = document.getElementById('js-type-area')
+
+axios({
+  method: 'GET',
+  url: 'https://pokeapi.co/api/v2/type'
+})
